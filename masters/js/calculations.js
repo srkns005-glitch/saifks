b5f44d9e3813fe277d9bdbcc835d76d99b4ae9fd
srkns005-glitch <srkns005@gmail.com>
2026-08-04 21:01:48 +0400

    Add files via upload

---
+++ masters/js/calculations.js
@@ -0,0 +1,7 @@
+export function calculateSummary(master,state){
+  return {
+    affinity:{available:0,required:0,remaining:0},
+    emblems:{available:0,required:0,remaining:0},
+    manuscripts:{available:0,required:0,remaining:0}
+  };
+}
