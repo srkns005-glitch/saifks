3c805c79112f92a36a9675c1121337ce38b3b12e
srkns005-glitch <srkns005@gmail.com>
2026-09-22 17:38:46 +0400

    Refresh Rally Planner cache after multilingual update

--- preview/sw.js
+++ preview/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="saifks-rally-v1";
+const CACHE_NAME="saifks-rally-v2";
 const ASSETS=["./","./rally.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",event=>event.waitUntil(self.clients.claim()));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./rally.html"))))});
