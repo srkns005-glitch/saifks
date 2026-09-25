44e5d580d1b6f6ec6380090052fab67c5070e3aa
srkns005-glitch <srkns005@gmail.com>
2026-09-26 00:23:05 +0400

    Translate all Hero Center skill names into Arabic

---
+++ hero-center/skill-titles-ar.js
@@ -0,0 +1,232 @@
+window.HERO_SKILL_TITLES_AR=Object.fromEntries(`
+Antler Assault|هجوم القرون
+Spear Charge|اندفاع الرمح
+Fury|الغضب
+Oath of Guardian|قسم الحارس
+Echoes of Valhalla|أصداء فالهالا
+Nature’s Balance|توازن الطبيعة
+Valiant Fury – Conquest Skill|الغضب الشجاع
+Zeal – Expedition Skill|الحماسة
+Combo Slash|الضربات المتتابعة
+Arcane Swordship|فن السيف السحري
+Onslaught|الهجوم الكاسح
+Battle Ready|الاستعداد للمعركة
+Way of the Blade|طريق النصل
+Unrighteous Strike|الضربة المباغتة
+Double Parry – Conquest Skill|الصد المزدوج
+Discernment – Expedition Skill|حسن التقدير
+Rapidﬁre|النيران السريعة
+Final Prayer|الصلاة الأخيرة
+Superior Techniques|تقنيات متفوقة
+Taskforce Training|تدريب فرقة المهام
+Resourceful|حسن استغلال الموارد
+Positional Batter|الضرب من الموقع
+Fearless Advance – Conquest Skill|التقدم بلا خوف
+Defend to Attack – Expedition Skill|الدفاع للهجوم
+Gallant Throw|الرمية الجريئة
+Brave the Storm|مواجهة العاصفة
+Persistence|المثابرة
+Rally Flag|راية الحشد
+Hero’s Domain|ميدان البطل
+Youthful Rage|حماسة الشباب
+Crimson Spirit – Conquest Skill|الروح القرمزية
+Divine Strength – Expedition Skill|القوة الإلهية
+Shield Strike|ضربة الدرع
+Agony Rush|اندفاع الألم
+Holy Might|القوة المقدسة
+Sundering Wound|الجرح القاطع
+Stoic|الثبات
+Inﬁnite Arsenal|الترسانة اللامتناهية
+Death or Glory – Conquest Skill|الموت أو المجد
+Dark Lady – Expedition Skill|سيدة الظلام
+Rejuvenating Plea|نداء التعافي
+Redeemer|المنقذ
+Intimidation|الترهيب
+Noble Path|الطريق النبيل
+Elixir of Strength|إكسير القوة
+Trial by Fire|اختبار النار
+Holy Invocation – Conquest Skill|الاستدعاء المقدس
+Fortitude – Expedition Skill|الصلابة
+Tidal Rush|اندفاع المد
+Fermented Blow|الضربة المخمّرة
+Lucky Hit|الضربة المحظوظة
+Wild Card|الورقة الرابحة
+Rumhead|عاشق الشراب
+Dynamo|القوة الدافعة
+Servant of Wine – Conquest Skill|خادم الشراب
+Admiral of the Line – Expedition Skill|قائد الصفوف
+Ground Shock|هزة الأرض
+Veterancy|خبرة المحارب
+Hammer of Justice|مطرقة العدالة
+Holy Warrior|المحارب المقدس
+Conviction|العزيمة
+Exhortation|التحفيز
+Blessed Hammer – Conquest Skill|المطرقة المباركة
+Vanguard – Expedition Skill|الطليعة
+Fatal Attraction|جاذبية قاتلة
+Game of Chance|لعبة الحظ
+Rumormonger|مروّج الشائعات
+The Tempest|العاصفة
+The Resistance|المقاومة
+The Celebration|الاحتفال
+Sound of Silence – Conquest Skill|صوت الصمت
+Hymn to Survival – Expedition Skill|نشيد النجاة
+Dichotomoy|الانقسام
+Change of Fate|تغيير المصير
+Turn Card|قلب الورقة
+Evil Eye|العين الشريرة
+The Favor|المنة
+The Shield|الدرع
+Weighted Deck – Conquest Skill|أوراق مرجّحة
+Cosmic Eye – Expedition Skill|العين الكونية
+Glorious Mark|العلامة المجيدة
+Boneshatter|تحطيم العظام
+Second Wind|نَفَس ثانٍ
+Rescuing Hands|أيادي الإنقاذ
+Praetorian Will|إرادة الحارس
+Capre Diem|اغتنام الفرصة
+Flagbearer – Conquest Skill|حامل الراية
+Vow of Honor – Expedition Skill|عهد الشرف
+Battle Focus|التركيز القتالي
+Flutterfang|الناب الخاطف
+Swoon Chakra|دوامة الإغماء
+Chaos Gambit|مناورة الفوضى
+Rose of War|وردة الحرب
+Golden Rhythm|الإيقاع الذهبي
+Night Reverie – Conquest Skill|حلم الليل
+Perihelion – Expedition Skill|الاقتراب من الشمس
+Ambush|الكمين
+Parry|الصد
+Stiletto|الخنجر
+Warbringer|جالب الحرب
+Subterfuge|الحيلة
+Sleight Hand|خفة اليد
+Indefensible – Conquest Skill|بلا دفاع
+Pugilist – Expedition Skill|المقاتل
+Ambrosia|شراب الآلهة
+Above and Beyond|تجاوز الحدود
+Mountain Mover|محرّك الجبال
+Mighty Paragon|المثال الأقوى
+Celestial Sustenance|مدد سماوي
+Art of War|فن الحرب
+Strong Brew – Conquest Skill|المشروب القوي
+Strategic Strike – Expedition Skill|الضربة الاستراتيجية
+Gilded Barrage|وابل ذهبي
+Lucky Strike|الضربة الموفقة
+Priceless Shot|الطلقة الثمينة
+Crouching Tiger|النمر المتربص
+Focus Fire|النيران المركزة
+Trap of Greed|فخ الجشع
+Reﬁned Gunplay – Conquest Skill|مهارة الرماية المتقنة
+Money Driven – Expedition Skill|حافز المال
+Axe Flurry|وابل الفؤوس
+Brutal Throw|الرمية العنيفة
+Feast of the Pack|وليمة القطيع
+Battle Hunger|نهم المعركة
+Reckless Charge|الاندفاع المتهور
+Ancestral Guidance|إرشاد الأسلاف
+Berserker’s Edge – Conquest Skill|حد الهائج
+Wolf-Kissed – Expedition Skill|بركة الذئب
+Tidewill|إرادة المد
+Regal Wrath|الغضب الملكي
+Striking Hit|ضربة قوية
+Command of Power|أمر القوة
+Warfare of Power|حرب القوة
+Oath of Power|قسم القوة
+Magnetic Reformation – Conquest Skill|إعادة التشكيل المغناطيسي
+Whale Call – Expedition Skill|نداء الحوت
+Kin Throw|رمية الأقارب
+Combo|الضربات المتتابعة
+Deadshot|الطلقة القاتلة
+Avalanche|الانهيار الجليدي
+Ice Zone|منطقة الجليد
+Quick Draw – Conquest Skill|السحب السريع
+Offenseive Defense – Expedition Skill|الدفاع الهجومي
+Puppet Master|سيد الدمى
+Scalding Mark|العلامة الحارقة
+Scalding Mark – Lash|لسعة العلامة الحارقة
+Arcane Pact|العهد السحري
+Terror – Deathblow|الرعب: الضربة القاضية
+Terror – Annihilation|الرعب: الإبادة
+Eyes of Obedience – Conquest Skill|عيون الطاعة
+Queen of Night – Expedition Skill|ملكة الليل
+Vengeance|الانتقام
+Parting Gift|هدية الوداع
+Nimble Soul|الروح الرشيقة
+Iron Bodies|الأجساد الحديدية
+Great Justice|العدالة الكبرى
+Mechanic – Conquest Skill|الميكانيكي
+Shock and Awe – Expedition Skill|الصدمة والرهبة
+High Ground|المرتفعات
+Incendiaries|المقذوفات الحارقة
+Frontal Support|الدعم الأمامي
+Artillerymen|رجال المدفعية
+Chain Shelling|القصف المتسلسل
+Boom Boom|انفجار متتالٍ
+Zero Sympathy – Conquest Skill|بلا رحمة
+Landmines – Expedition Skill|الألغام الأرضية
+Color Bomb|قنبلة الألوان
+Corruption Bomb|قنبلة الفساد
+Disguise Pigment|صبغة التمويه
+Dissolution|الذوبان
+Chiaroscuro|تباين الضوء والظل
+Light and Cold|الضوء والبرودة
+Irradiance – Conquest Skill|الإشعاع
+Color Storm – Expedition Skill|عاصفة الألوان
+Petalbloom|تفتح البتلات
+Falling Swallow|السنونو الهابط
+Twin Fangs|النابان التوأم
+On Guard|على أهبة الاستعداد
+Well-Traveled|واسع الخبرة
+Incendiary Arrows|السهام الحارقة
+Sharpshooter|القناص
+Deadly Strike|الضربة المميتة
+Iron Constitution|البنية الحديدية
+Quick Paced|الخطوات السريعة
+Headshot|إصابة الرأس
+Quick Shot|الطلقة السريعة
+Natural Selection|الانتقاء الطبيعي
+Sixth Sense|الحاسة السادسة
+Precision Shot|الطلقة الدقيقة
+Shielded Strike|الضربة المحمية
+Joint Defense|الدفاع المشترك
+Shield Block|صد الدرع
+Defenders’ Edge|ميزة المدافعين
+Weaken|الإضعاف
+BBQ Feast|وليمة الشواء
+Culinary Chaos|فوضى الطهي
+Emergency Snack|وجبة الطوارئ
+Super Nutrients|المغذيات الفائقة
+Trash Talk|الاستفزاز
+Sandstorm|العاصفة الرملية
+Eagle Wings|أجنحة النسر
+Dune General|قائد الكثبان
+Desert Eclipse|كسوف الصحراء
+Pathﬁnder|مستكشف الطريق
+Sakura Blossom|تفتح الساكورا
+Fire Sigil|ختم النار
+Divine Melody|اللحن الإلهي
+Tri-Phalanx|التشكيل الثلاثي
+Exorcism|طرد الأرواح
+Burst Fire|النيران المتفجرة
+Defense Upgrade|تعزيز الدفاع
+Weapon Upgrade|تطوير السلاح
+Stand of Arms|الموقف المسلح
+Shield Wall|جدار الدروع
+Hammer Burn|ضربة المطرقة الحارقة
+Armor Enhancement|تعزيز الدرع
+Burnished Iron|الحديد المصقول
+Craftsmanship|الإتقان الحرفي
+Axe Whirl|دوامة الفأس
+Razor Sharp|الحد القاطع
+Woodland Inheritor|وارث الغابة
+Master Woodcutter|الحطاب الماهر
+Shrapnel Load|حمولة الشظايا
+Detonate|التفجير
+Demolitions Expert|خبير المتفجرات
+Stone Mining|تعدين الحجر
+Rain of Arrows|وابل السهام
+Target Lock|تثبيت الهدف
+Green Thumb|مهارة الزراعة
+Forager’s Luck|حظ الباحث عن المؤن
+`.trim().split('\n').map(line=>{const divider=line.indexOf('|');return [line.slice(0,divider),line.slice(divider+1)]}));
